5ce805d272936459a095c07f885c4aa872bce955
Henrique Pereira <henrique.anacretto.pereira@gmail.com>
2018-08-26 19:37:48 -0300

    Criando rotas de autenticacao

---
+++ app/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const requireDir = require('require-dir');
+
+const routes = express.Router();
+
+const controllers = requireDir('./controllers');
+
+/**
+ * Auth
+ */
+routes.post('/signup', controllers.authController.signup);
+routes.post('/signin', controllers.authController.signin);
+
+module.exports = routes;
